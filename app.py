d8ccf8d0a57ae71b847d45402b814e0f01c717eb
KesterDaniel <kesterdan17@gmail.com>
2023-01-06 21:05:20 +0100

    added two random cards for each player when game starts

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 # blackjack capstone project
+import random
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def play():
+  my_cards = []
+  comp_cards = []
+
+  for i in range(2):
+    my_cards.append(random.choice(cards))
+    comp_cards.append(random.choice(cards))
+
+  print(my_cards)
+  print(comp_cards)
+
+play()
